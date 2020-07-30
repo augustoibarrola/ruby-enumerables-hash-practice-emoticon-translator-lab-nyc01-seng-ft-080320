522e7e630e1dec5d0eb1012a50e1783dcc405377
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-30 22:57:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -58,7 +58,7 @@ def get_english_meaning(yaml_file, emoticon)
   end
   binding.pry
   if together_now.flatten.include? emoticon
-    emoticon_set.key(key)
+    emoticon_set.key(key.first)
   else
     sorry_message
   end
